0461c4fcec1e787d59cfdd5c2bd3ec8748d09313
Filios92 <filios92@gmail.com>
2026-05-20 16:59:24 +0200

    nvim-spider

--- init.lua
+++ init.lua
@@ -97,6 +97,15 @@ require('lazy').setup({
   },
 
   { 'NMAC427/guess-indent.nvim', cond = not vim.g.vscode, opts = {} },
+  {
+    'chrisgrieser/nvim-spider',
+    keys = {
+      { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'n', 'o', 'x' } },
+      { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'n', 'o', 'x' } },
+      { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'n', 'o', 'x' } },
+      { 'ge', "<cmd>lua require('spider').motion('ge')<CR>", mode = { 'n', 'o', 'x' } },
+    },
+  },
 
   { -- Collection of various small independent plugins/modules
     'nvim-mini/mini.nvim',
